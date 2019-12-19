e49a7d14ab8fdf949f94b6acf01a08fd91b00e8f
Chen Haoran <chenhaoran.earth@live.com>
2019-12-19 17:31:34 +0800

    update run_model.sh

--- msvd/run_model.sh
+++ msvd/run_model.sh
@@ -2,10 +2,10 @@ TF_XLA_FLAGS=--tf_xla_cpu_global_jit \
 XLA_FLAGS=--xla_hlo_profile \
 CUDA_VISIBLE_DEVICES=1 \
 python train_model.py --name test \
-    --corpus /data2/chenhaoran/video_feats/msvd_corpus_glove.pkl \
-    --ecores /data2/chenhaoran/video_feats/msvd_eco_res_avg_norm.npy \
-    --tag    /data2/chenhaoran/video_feats/msvd_semantic_tag_eco_res_avg.npy \
-    --ref    /data2/chenhaoran/video_feats/msvd_ref3.pkl \
+    --corpus /PATH/TO/FILE/msvd_corpus_glove.pkl
+    --ecores /PATH/TO/FILE/msvd_eco_res_avg_norm.npy \
+    --tag    /PATH/TO/FILE/msvd_semantic_tag_eco_res_avg.npy \
+    --ref    /PATH/TO/FILE/video_feats/msvd_ref3.pkl \
     --test   ./saves/lr2_msvd_eco_res_avg_norm_semantic_tag_eco_res_avg_ss16-best.ckpt \
     > test.log
     
